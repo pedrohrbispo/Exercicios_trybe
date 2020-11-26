de79a05140694dd896cb4a34a45eb7ad11e4bef2
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-26 12:11:23 -0300

    Faz programa que calcula maior de 3 variáveis

--- bloco_4/dia_1/scripts.js
+++ bloco_4/dia_1/scripts.js
@@ -1,6 +1,9 @@
-let a=5,b=2;
-if( a>b ){
+let a=5,b=2,c=9;
+
+if( a>= b && a >= c) {
     console.log(a);
-} else {
+} else if( b >= a && b>= c ) {
     console.log(b);
+} else if( c >= a && c>= b ) {
+    console.log(c);
 }
