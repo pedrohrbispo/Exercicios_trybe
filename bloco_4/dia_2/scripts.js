4531429e926857d034c2d4e8434997210282e9ff
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-26 18:37:24 -0300

    Faz a media aritimetica do array

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -1,7 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma=0;
+let soma=0, numElementos=0;
 for(let index = 0; index < numbers.length; index += 1){
     soma=soma+numbers[index];
+    numElementos +=1;
 }
 
-console.log(soma);
+console.log(soma/numElementos);
