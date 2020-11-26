14daf31ee8cd0e3902222bade58e19e1d6675057
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-26 18:57:30 -0300

    Faz um array que vai de 1 a 25 e divide todos os elementos por 2

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -3,5 +3,5 @@ for(let index = 0; index < 25; index += 1){
 numbers.push(index+1);
 }
 for(let index = 0; index < numbers.length; index += 1){
-    console.log(numbers[index]);
+    console.log(numbers[index]/2);
     }
