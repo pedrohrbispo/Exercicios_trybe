4e2c8f760d521382a6a73fef721c7d2d14deb285
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-26 18:48:47 -0300

    Encontra a quantidade de numeros ímpares

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -1,9 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior= numbers[0];
+let qtdImpar=0;
 
 for(let index = 0; index < numbers.length; index += 1){
-    if(numbers[index] > maior){
-        maior=numbers[index];
+    if(numbers[index] % 2==1){
+        qtdImpar+=1;
     }
 }
-console.log(maior);
+if(qtdImpar > 0){
+    console.log('A quantidade de numeros impares é ' + qtdImpar);
+} else {
+    console.log('Nenhum valor encontrado');
+}
