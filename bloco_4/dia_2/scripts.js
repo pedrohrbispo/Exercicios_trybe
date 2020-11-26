89e406534b929ad5dc4027fe922c28189de1cfc5
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-26 18:43:02 -0300

    Encontra o maior valor do array

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -1,12 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma=0, numElementos=0;
-for(let index = 0; index < numbers.length; index += 1){
-    soma=soma+numbers[index];
-    numElementos +=1;
-}
+let maior= numbers[0];
 
-if((soma/numElementos) > 20 ){
-    console.log('Valor maior que 20');
-} else {
-    console.log('Valor menor ou igual a 20');
+for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index] > maior){
+        maior=numbers[index];
+    }
 }
+console.log(maior);
