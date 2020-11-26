b4cf8318ae1524584aea5fce1e507383d365cdbe
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-26 18:50:52 -0300

    Encontra o menor valor do array

--- bloco_4/dia_2/scripts.js
+++ bloco_4/dia_2/scripts.js
@@ -1,13 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let qtdImpar=0;
+let menor= numbers[0];
 
 for(let index = 0; index < numbers.length; index += 1){
-    if(numbers[index] % 2==1){
-        qtdImpar+=1;
+    if(numbers[index] < menor){
+
+        menor=numbers[index];
     }
 }
-if(qtdImpar > 0){
-    console.log('A quantidade de numeros impares é ' + qtdImpar);
-} else {
-    console.log('Nenhum valor encontrado');
-}
+console.log(menor);
