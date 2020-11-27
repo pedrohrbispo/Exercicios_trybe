a073aa8251cfc0b58e8b6adda6f1843380dea993
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-27 16:28:48 -0300

    Penultimo passo da piramide que deixa ela crescente

--- bloco_4/dia_3/scripts.js
+++ bloco_4/dia_3/scripts.js
@@ -1,14 +1,11 @@
-let n=5;
+let n=5, parametro=0;
 let asterisco = ''
 for(let index=0; index < n; index += 1){
-    for(let index_2= n-1; index_2 >0; index_2 -= 1){
-       asterisco= asterisco + ' ';
+    for(let index_2 = 0 ; index_2 <=parametro; index_2 += 1){
+       asterisco = asterisco + '*';
         
     }
-    for(index_3 = 0 ;index_3 < n; index_3 += 1){
-
-        asterisco=asterisco+'*'
-    }
     console.log(asterisco);
-    asterisco='';
+    parametro += 1;
+    asterisco = '';
 }
