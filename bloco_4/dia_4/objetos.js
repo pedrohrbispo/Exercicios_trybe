911f2304449a6dd83072dd7bd7fecc2478b814d7
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-30 21:04:10 -0300

    Inicia o uso do for in

--- bloco_4/dia_4/objetos.js
+++ bloco_4/dia_4/objetos.js
@@ -4,11 +4,12 @@ let info = {
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
 
-//   for( let key in info){ 
-//     console.log(info.)
-//   }
-console.log('Bem Vinda, ' + info.personagem);
+  console.log('Bem Vinda, ' + info.personagem);
 
 info.recorrente = 'Sim';
 
 console.log (info);
+
+  for( let key in info){ 
+    console.log(key);
+  }
