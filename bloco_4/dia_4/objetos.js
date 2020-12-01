5b5f65c960fe856f283e9d1488997e9af5892730
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-30 21:11:40 -0300

    Termina a parte de objetos e for in

--- bloco_4/dia_4/objetos.js
+++ bloco_4/dia_4/objetos.js
@@ -13,3 +13,12 @@ console.log (info);
   for( let key in info){ 
     console.log(info[key]);
   }
+
+  let info2 = {
+      personagem : info.personagem + ' e Tio patinhas',
+      origem : info.origem + 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+      nota : info.nota + 'O último MacPatinhas', 
+      recorrente : 'Ambos recorrentes',
+  }
+
+  console.log(info2);
