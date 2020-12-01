9aea23bc1e102f2bc5c0126f4906380ddeb4a2af
Pedro Henrique <pedroenrique.400@gmail.com>
2020-11-30 22:38:24 -0300

    faz uma função que retorna o maior nome de um array de nomes

--- bloco_4/dia_4/objetos.js
+++ bloco_4/dia_4/objetos.js
@@ -1,17 +1,18 @@
-function indexOfTheHighiest( numbers) {
-    let maior= numbers[0];
-    for( let index = 0; index < numbers.length; index +=1){
-        if(numbers[index] > maior){
-            maior = numbers[index];
-        }
+let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; 
 
+function highiestName(names){
+    let sizes = [], position=0
+    let maior=0
+   for(let index=0; index < names.length; index +=1){
+       sizes.push (names[index].length);
+   }
+   for(let index=0; index < sizes.length; index +=1){
+        if(sizes[index]> maior){
+            maior=sizes[index];
+            position=index;
+        }
     }
-    for( let index = 0; index < numbers.length; index +=1) {
-        if(maior == numbers[index]){
-            return index;
-        }
-    }  
+    return names[position];
 }
-let array = [2, 3, 6, 7, 10, 1];
 
-console.log(indexOfTheHighiest(array));
+console.log(highiestName(array));
