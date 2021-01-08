2abf54fc227fcabafe32d50e08f53e7d78fbdb23
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-08 13:08:12 -0300

    Deleta arquivos para organizar a branch

--- bloco_4/dia_2/scripts.js
+++
@@ -1,7 +0,0 @@
-let numbers = [];
-for(let index = 0; index < 25; index += 1){
-numbers.push(index+1);
-}
-for(let index = 0; index < numbers.length; index += 1){
-    console.log(numbers[index]/2);
-    }
