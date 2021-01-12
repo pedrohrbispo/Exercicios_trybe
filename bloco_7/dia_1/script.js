9babab1efeb9cfa8811a5685d00122c01447ca62
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-12 17:59:26 -0300

    Faz o exercício de ordenação e template literals

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -1,12 +1,7 @@
- const testingScope = (escopo) => { 
-    if (escopo === true) { 
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-  }
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  oddsAndEvens.sort(function(a,b) {
+  return a-b; 
+  });
+  // Seu código aqui.
 
-  testingScope(true);
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
