09fb2f834ab0fa91e4596eecb0b795d424315767
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-27 20:26:31 -0300

    inicia os estudos da função setTimeout exercício 5

--- bloco_9/dia_1/script.js
+++ bloco_9/dia_1/script.js
@@ -1,12 +1,15 @@
-const getPlanet = () => {
-    const mars = {
-      name: "Mars",
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: "kilometers",
-      },
-    };
-    console.log("Returned planet: ", mars);
-  };
-  
- setTimeout(() => getPlanet(), 4000);  // imprime Marte depois de 4 segundos
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+    let timeout = Math.floor(Math.random() * (5000 - 1000 + 1) + 1000);
+    setTimeout(() => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), timeout);
+}
+
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
